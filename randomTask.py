7819eafe0021b4a3b6a91335e1592a5e9877a76a
nuvhandra <k3ll3x@gmail.com>
2021-03-04 14:01:59 -0600

    remove regex from html tags in randomTask

--- randomTask.py
+++ randomTask.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import os, json, random
+import os, json, random, re
 
 filename = "/home/nuvhandra/src/Ruby-ERB-Sinatra-Task-Scheduler/public/history.json"
 f = open(filename, "r")
@@ -8,4 +8,6 @@ f.close()
 rnd_inx = random.randint(0, len(tasks))
 rnd_task = tasks[rnd_inx]
 
-os.popen("notify-send -i /home/nuvhandra/Pictures/xek.png '" + rnd_task['comd'] + "'")
+rnd_task = re.sub("<.+?>", "", rnd_task['comd'])
+
+os.popen("notify-send -i /home/nuvhandra/Pictures/xek.png '" + rnd_task + "'")
